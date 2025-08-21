7aad8b76ece03c011938eff84eb67bb055116fb1
root <root@TUKTUK>
2025-08-22 00:37:30 +0900

    update-models

--- agents/team4_agent/agents/model.py
+++ agents/team4_agent/agents/model.py
@@ -2,4 +2,6 @@ from google.adk.models.lite_llm import LiteLlm
 
 MODEL_GEMINI_2_5_LITE = "gemini-2.5-flash-lite"
 MODEL_GPT_4_MINI = LiteLlm(model="openai/gpt-4o-mini")
+MODEL_GPT_o1 = LiteLlm(model="openai/o1")
+MODEL_GPT_5_mini = LiteLlm(model="openai/gpt-5-mini")
 AGENT_MODEL = MODEL_GPT_4_MINI
